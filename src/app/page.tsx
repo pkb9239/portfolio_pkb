e98a4fe1e9507177b1a9ff9c2c8ceeb54ea356e7
Kyungbin <pkb8839@naver.com>
2024-04-03 16:57:40 +0900

    🔧chore : tailwind 세팅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1>hi</h1>;
+  return (
+    <>
+      <h4 className={'text-red-600'}>Hello World</h4>
+      <div className="bg-color-1 hidden"></div>
+    </>
+  );
 }
