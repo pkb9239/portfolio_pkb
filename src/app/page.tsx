544575fc15ca73290bde4391886b20e5a1836f42
Kyungbin <pkb8839@naver.com>
2024-04-04 15:51:21 +0900

    🐛fix : tailwind 적용이 안되는 문제
    layout.tsx에 global.css를 import해와서 해결했습니다.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <>
       <h4 className={'text-red-600'}>Hello World</h4>
       <div className="bg-color-1 hidden"></div>
+      <div className="text-sky-500 dark:text-sky-400">Sarah Dayan</div>
     </>
   );
 }
