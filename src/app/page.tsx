b5aa2a2412a58a2885c8ddcf7eac30b8e1e13b77
Kyungbin <pkb8839@naver.com>
2024-04-10 16:35:14 +0900

    ✨feat : tailwind 애니메이션 추가
    tailwind.config.ts를 수정하여 애니메이션을 추가하였습니다.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,54 @@
+import Header from '@/components/Header';
+import NavBar from '@/components/NavBar';
+
 export default function Home() {
   return (
     <>
-      <h4 className={'text-red-600'}>Hello World</h4>
-      <div className="bg-color-1 hidden"></div>
-      <div className="text-sky-500 dark:text-sky-400">Sarah Dayan</div>
+      <NavBar />
+      <Header />
+      <div className="px-4">
+        <div
+          id="about-me"
+          className="py-50"
+        >
+          About me section content
+        </div>
+        <div>hello</div>
+        <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{' '}
+        <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{' '}
+        <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{' '}
+        <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{' '}
+        <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{' '}
+        <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{' '}
+        <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{' '}
+        <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{' '}
+        <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{' '}
+        <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{' '}
+        <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{' '}
+        <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{' '}
+        <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{' '}
+        <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{' '}
+        <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{' '}
+        <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{' '}
+        <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{' '}
+        <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{' '}
+        <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{' '}
+        <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{' '}
+        <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{' '}
+        <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{' '}
+        <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{' '}
+        <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{' '}
+        <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{' '}
+        <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{' '}
+        <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{' '}
+        <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{' '}
+        <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{' '}
+        <div>hello</div> <div>hello</div> <div>hello</div>
+        <div id="skills">Skills section content</div>
+        <div id="archiving">Archiving section content</div>
+        <div id="project">Project section content</div>
+        <div id="career">Career section content</div>
+      </div>
     </>
   );
 }
