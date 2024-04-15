69c487e2ed0086b504f5b15de2aefc9f8dbeebd9
Kyungbin <pkb8839@naver.com>
2024-04-15 18:05:21 +0900

    ✨feat : ScrollTopButton 컴포넌트 적용
    모바일일때를 고려하여 position 값을 다르게 설정하였습니다.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import Header from '@/components/Header';
 import NavBar from '@/components/NavBar';
+import ScrollTopButton from '@/components/ScrollTopButton';
 
 export default function Home() {
   return (
     <>
       <NavBar />
       <Header />
+      <div className="fixed md:right-12 md:bottom-12 right-5 bottom-5 flex justify-around items-center w-12 h-12">
+        <ScrollTopButton />
+      </div>
+
       <div className="px-4">
         <div
           id="about-me"
