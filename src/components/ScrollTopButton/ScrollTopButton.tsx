d90ceceb16dea70eb5538abf12e16943508aee35
Kyungbin <pkb8839@naver.com>
2024-04-15 17:55:32 +0900

    ✨feat : ScrollTopButton 컴포넌트 구현
    - 모바일 고려하여 반응형으로 제작하였습니다.
    - react-scroll의 Link 이용하여 클릭시 최상단으로 이동할 수 있게 구현하였습니다.

--- src/components/ScrollTopButton/ScrollTopButton.tsx
+++ src/components/ScrollTopButton/ScrollTopButton.tsx
@@ -1,7 +1,40 @@
+'use client';
+import {
+  OPENMOBILENAVBARHEIGHTOFFSET,
+  PCNAVBARHEIGHTOFFSET,
+} from '@/constants';
+import useResize from '@/hooks/useResize';
+import { Link } from 'react-scroll';
+
 const ScrollTopButton = () => {
+  const { isMobile } = useResize();
+  const offset = isMobile ? OPENMOBILENAVBARHEIGHTOFFSET : PCNAVBARHEIGHTOFFSET;
   return (
     <div>
-      <button>스크롤 탑</button>
+      <Link
+        to="#"
+        smooth={true}
+        offset={offset}
+        onClick={() => {
+          console.log('ho');
+        }}
+        className="flex justify-around items-center w-10 h-10 md:w-14 md:h-14 rounded-lg bg-stone-100 shadow-xl md:hover:bg-slate-600 md:hover:text-white"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="2"
+          stroke="currentColor"
+          className="md:w-9 md:h-9 w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18"
+          />
+        </svg>
+      </Link>
     </div>
   );
 };
