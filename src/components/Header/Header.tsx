55ac3f98eb82f6b8f55afcb2de8e3ea66c3c6e5d
Kyungbin <pkb8839@naver.com>
2024-04-17 13:29:22 +0900

    💄design : Header 컴포넌트 스타일 변경
    배경이미지를 추가하였습니다.
    스크롤시 배경만 내려가도록 하였습니다.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,6 @@
 
 import { MOBILENAVBARHEIGHTOFFSET, PCNAVBARHEIGHTOFFSET } from '@/constants';
 import useResize from '@/hooks/useResize';
-import { useEffect, useState } from 'react';
 import { Link } from 'react-scroll';
 
 const Header = () => {
@@ -15,55 +14,62 @@ const Header = () => {
   return (
     <div
       id="#"
-      className="flex flex-col md:gap-5 gap-3 items-center w-full  h-full p-5 md:p-8 bg-slate-600"
+      className="flex flex-col md:gap-5 gap-3 items-center w-full h-screen p-5 md:p-8 bg-fixed relative"
+      style={{
+        backgroundImage: "url('/images/pkb.jpeg')",
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+      }}
     >
-      <div className="md:text-4xl text-xl text-white">
-        프론트엔드 개발자 l 박경빈
-      </div>
-      <div className="flex flex-col items-center justify-center md:gap-3 gap-1.5 text-white">
-        <div className="flex gap-1">
-          <div className="animate-[wiggle_1s_ease-in-out_infinite] md:text-xl text-l">
-            👋🏻
+      <div className="absolute inset-0 backdrop-opacity-10 backdrop-invert bg-white/30"></div>
+      <div className="flex flex-col w-full h-full relative z-10 items-center justify-center">
+        <div className="flex flex-col items-center justify-center md:gap-10 gap-1.5 font-semibold">
+          <div className="flex gap-1">
+            <div className="animate-[wiggle_1s_ease-in-out_infinite] md:text-3xl text-xl font-semibold">
+              👋🏻
+            </div>
+            <div className="md:text-3xl text-xl"> 안녕하세요.</div>
           </div>
-          <div className="md:text-xl text-l">안녕하세요.</div>
+          <p className="md:text-3xl text-xl">🌳 울창한 숲을 목표로</p>
+          <p className="md:text-3xl text-xl">🪵 한 그루 한 그루 나무를 심는</p>
+          <p className="md:text-3xl text-xl">
+            🧑🏻‍💻 프론트엔드 개발자 박경빈입니다.
+          </p>
+          <Link
+            to="about-me"
+            smooth={true}
+            offset={navbarHeightOffset}
+            className="block px-2 py-2 text-sm cursor-pointer"
+          >
+            <svg
+              className="animate-bounce w-8 h-8"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2.5"
+                d="M19 9l-7 7-7-7"
+              />
+            </svg>
+            <svg
+              className="animate-bounce w-8 h-8 text-blue-500"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2.5}
+                d="M19 9l-7 7-7-7"
+              />
+            </svg>
+          </Link>
         </div>
-        <p className="md:text-xl text-l">
-          🌳 울창한 숲을 목표로 한 그루 한 그루 나무를 심는
-        </p>
-        <p className="md:text-xl text-l">🧑🏻‍💻 프론트엔드 개발자 박경빈입니다.</p>
-        <Link
-          to="about-me"
-          smooth={true}
-          offset={navbarHeightOffset}
-          className="block px-2 py-2 text-sm cursor-pointer"
-        >
-          <svg
-            className="animate-bounce w-6 h-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M19 9l-7 7-7-7"
-            />
-          </svg>
-          <svg
-            className="animate-bounce w-6 h-6 text-blue-500"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M19 9l-7 7-7-7"
-            />
-          </svg>
-        </Link>
       </div>
     </div>
   );
