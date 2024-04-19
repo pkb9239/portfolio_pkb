399488f491f054773faed56533d8c9d8d922ad7b
Kyungbin <pkb8839@naver.com>
2024-04-19 13:06:13 +0900

    ✨feat : Title 컴포넌트 구현
    props로 title에 들어갈 text를 받습니다.

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,1 +1,12 @@
-export { default } from './index';
+const Title = ({ text }: { text: string }) => {
+  return (
+    <>
+      <div className="flex flex-col md:gap-3 gap-1">
+        <div className="md:text-7xl text-3xl">{text}</div>
+        <div className="w-3/5 md:border-t-8 border-t-4 border-slate-600"></div>
+      </div>
+    </>
+  );
+};
+
+export default Title;
