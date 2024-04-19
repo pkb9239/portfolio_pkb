ff6c338315ca98092b8a1fb6239f40ba9ce3ce7d
Kyungbin <pkb8839@naver.com>
2024-04-19 16:57:13 +0900

    ✨feat : Link 태그 속성 추가
    noopener - 이 값은 새로 열린 페이지에서 원래 페이지를 참조하지 못하게 합니다. 이렇게 하면 새로 열린 페이지가 원래 페이지에 액세스하는 것을 방지하여 보안 문제를 예방할 수 있습니다.
    noreferrer - 이 값은 새로 열린 페이지에서 자신을 참조하는 헤더 정보를 제거합니다. 이렇게 하면 원래 페이지의 정보가 새로 열린 페이지에 전달되는 것을 방지합니다. 이 역시 보안 문제를 예방하는 데 도움이 됩니다.
    target="_blank" - 링크를 새 탭이나 새 창에서 열도록 지시합니다. 이는 사용자가 원래 페이지를 계속 볼 수 있게 하면서 새로운 링크도 볼 수 있게 해줍니다.
    passHref - Next.js의 Link 컴포넌트에서 사용될 때, 내부에 있는 자식 컴포넌트에 href 속성을 전달하기 위해 필요합니다.
    AboutMe 헤더의 글자크기와 굵기를 수정하였습니다.

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -23,7 +23,7 @@ const AboutMe = () => {
             d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Zm6-10.125a1.875 1.875 0 1 1-3.75 0 1.875 1.875 0 0 1 3.75 0Zm1.294 6.336a6.721 6.721 0 0 1-3.17.789 6.721 6.721 0 0 1-3.168-.789 3.376 3.376 0 0 1 6.338 0Z"
           />
         </svg>
-        <span className="text-xl text-center">ABOUT ME</span>
+        <span className="text-2xl font-semibold text-center">ABOUT ME</span>
       </div>
       <div className="w-4/5">
         <div className="w-full border-t-2 border-slate-600"></div>
@@ -43,6 +43,8 @@ const AboutMe = () => {
           <Link
             className="flex gap-1 items-center justify-center py-2 transition ease-in-out delay-150 hover:bg-aside-second-bg hover:-translate-y-1 hover:scale-110 duration-300"
             href="https://github.com/pkb9239"
+            rel="noopener noreferrer"
+            target="_blank"
             passHref
           >
             <AiFillGithub size={24} />
@@ -52,6 +54,8 @@ const AboutMe = () => {
           <Link
             className="flex gap-1 items-center justify-center py-2 transition ease-in-out delay-150 hover:bg-aside-second-bg hover:-translate-y-1 hover:scale-110 duration-300"
             href="https://velog.io/@pkb0711/posts"
+            rel="noopener noreferrer"
+            target="_blank"
             passHref
           >
             <SiVelog size={24} />
