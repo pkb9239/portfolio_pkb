d88f85718985851e53adf195528e32e2c03d228e
Kyungbin <pkb8839@naver.com>
2024-04-22 22:50:04 +0900

    ✨feat : Content 컴포넌트 구현
    Content 컴포넌트는 총 7가지의 props를 가집니다.
    category - 필수) string 타입
    title - 필수) ReactNode 타입
    content - 필수) ReactNode 배열 타입
    introduce - 선택) string 타입
    period - 선택) string 타입
    skill - 선택) ReactNode 타입
    links - 선택) ReactNode 배열 타입

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,5 +1,94 @@
-const Content = () => {
-  return <div></div>;
+import { ReactNode } from 'react';
+import { FaCheck } from 'react-icons/fa';
+import { RiAttachment2 } from 'react-icons/ri';
+import { BsStack } from 'react-icons/bs';
+interface ContentProps {
+  category: string;
+  title: ReactNode;
+  introduce?: string;
+  period?: string;
+  content: ReactNode[];
+  links?: ReactNode[];
+  skill?: ReactNode;
+}
+
+const Content = ({
+  category,
+  title,
+  introduce,
+  period,
+  content,
+  skill,
+  links,
+}: ContentProps) => {
+  return (
+    <>
+      <div className="flex flex-col gap-4">
+        <div className="text-2xl font-medium">{category}</div>
+        <div className="text-5xl font-bold">{title}</div>
+        <div>{period && <div className="text-lg">{period}</div>}</div>
+        <div>
+          {period && <div className="text-lg font-medium">{introduce}</div>}
+        </div>
+        <div>
+          <ul>
+            {content.map((item, index) => (
+              <li
+                className="flex gap-2 text-xl items-center pl-2"
+                key={index}
+              >
+                <span>
+                  <FaCheck
+                    color="#3b81f6"
+                    size={20}
+                  />
+                </span>
+                {item}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          {links && (
+            <div className="flex items-center text-2xl gap-1 font-bold pb-4">
+              <span>
+                <RiAttachment2 size={24} />
+              </span>
+              관련 링크
+            </div>
+          )}
+          <ul>
+            {links &&
+              links.map((item, index) => (
+                <li
+                  className="flex gap-2 text-xl items-center pl-2"
+                  key={index}
+                >
+                  <span>
+                    <FaCheck
+                      color="#3b81f6"
+                      size={20}
+                    />
+                  </span>
+                  {item}
+                </li>
+              ))}
+          </ul>
+        </div>
+        <div>
+          {skill && (
+            <div className="flex items-center gap-1 text-2xl font-bold pb-4">
+              <span>
+                <BsStack size={22} />
+              </span>
+              기술 스택
+            </div>
+          )}
+          {skill && <div>{skill}</div>}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Content;
