7d5118e4b5d81d9ee4c360d7efed5bc8530671f4
Kyungbin <pkb8839@naver.com>
2024-04-22 23:20:37 +0900

    💄design : Title 스타일링 변경
    width값을 full로 수정하였습니다.

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,9 +1,9 @@
 const Title = ({ text }: { text: string }) => {
   return (
     <>
-      <div className="flex flex-col md:gap-3 gap-1">
+      <div className="flex flex-col md:gap-6 gap-1">
         <div className="md:text-7xl text-3xl">{text}</div>
-        <div className="w-3/5 md:border-t-8 border-t-4 border-slate-600"></div>
+        <div className="w-full md:border-t-8 border-t-4 border-slate-600"></div>
       </div>
     </>
   );
