cd1f3578a92e5e27a4e6ff947216e13ad1b3e593
Kyungbin <pkb8839@naver.com>
2024-04-25 15:17:24 +0900

    ✨feat : KiwingProject 컴포넌트 구현
    KiwingProject 컴포넌트의 Content와 Accordions의 props로 들어갈 content를 constants/kiwing.tsx 파일로 분리하였습니다.

---
+++ src/components/Project/KiwingProject.tsx
@@ -0,0 +1,52 @@
+import Link from 'next/link';
+import Content from '../Content';
+import Title from '../Title';
+import { kiwingAccordionsContent, kiwingContent } from '@/constants/kiwing';
+import Accordions from '../Accordions';
+
+const KiwingProject = () => {
+  return (
+    <div className="flex flex-col gap-10">
+      <Content
+        category="팀 프로젝트"
+        title={
+          <Link
+            href="https://kiwing.kr/"
+            rel="noopener noreferrer"
+            target="_blank"
+            passHref
+            className="underline transition-colors duration-300 hover:bg-main-color"
+          >
+            kiwing
+          </Link>
+        }
+        introduce="사용자들에게 면접을 효율적이고 빠르게 준비할 수 있는 서비스를 개발하였습니다. 면접을 준비하는 과정에서 질문과 답변의 파편화에 불편함을 느껴 한 사이트에서 검색부터 질문, 답변 작성까지 한번에 같이 할 수 있으면 좋겠다 생각하여 기획하게 되었습니다."
+        period="2024년 1월 - 진행중"
+        content={kiwingContent}
+        links={[
+          <Link
+            className="underline transition-colors duration-300 hover:bg-main-color"
+            href="https://github.com/Team-kiwing/Team-3seco-kiwing-fe"
+            rel="noopener noreferrer"
+            target="_blank"
+            passHref
+          >
+            GitHub Repository
+          </Link>,
+          <Link
+            className="underline transition-colors duration-300 hover:bg-main-color"
+            href="https://prgrms.notion.site/2794be1a25474d5a8ce0bed8b3191539"
+            rel="noopener noreferrer"
+            target="_blank"
+            passHref
+          >
+            키윙 기획서 노션
+          </Link>,
+        ]}
+        skill={<Accordions content={kiwingAccordionsContent} />}
+      />
+    </div>
+  );
+};
+
+export default KiwingProject;
