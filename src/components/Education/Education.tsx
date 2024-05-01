95a20b18bac49b9f715eac57dd42d03208d157cc
Kyungbin <pkb8839@naver.com>
2024-05-01 14:52:50 +0900

    ✨feat : Content 및 기간 추가
    - 동아리에 관한 내용인 새로운 Content를 추가하였습니다.
    - 모든 Content에 기간을 추가하였습니다.

--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -13,6 +13,7 @@ const Education = () => {
       />
       <Content
         category="교육"
+        period="2023년 9월 - 2024년 3월"
         title={
           <Link
             href="https://school.programmers.co.kr/learn/courses/17920/17920-5%EA%B8%B0-k-digital-training-%EB%B9%85%EB%8D%B0%EC%9D%B4%ED%84%B0-%ED%94%8C%EB%9E%AB%ED%8F%BC-%ED%94%84%EB%A1%A0%ED%8A%B8%EC%97%94%EB%93%9C-%EC%97%94%EC%A7%80%EB%8B%88%EC%96%B4%EB%A7%81"
@@ -29,6 +30,34 @@ const Education = () => {
           '함수형 프로그래밍에 대해 학습했습니다.',
           'VanillaJS로 프로젝트를 진행했습니다',
           'React, Vue 학습 및 프로젝트를 진행했습니다.',
+        ]}
+      />
+      <Content
+        category="동아리"
+        period="2023년 3월 - 2023년 8월"
+        title={
+          <Link
+            href="https://github.com/LikeLion-11th-JBU"
+            rel="noopener noreferrer"
+            target="_blank"
+            passHref
+            className="underline transition-colors duration-300 hover:bg-main-color"
+          >
+            멋쟁이 사자처럼 11기
+          </Link>
+        }
+        content={[
+          '웹 개발 기초를 다졌습니다.',
+          'HTML/CSS, Javascript, React를 다양한 과제를 수행하면서 실력을 향상시켰습니다.',
+          <Link
+            href="https://github.com/LikeLion-11th-JBU/elgineer-team-be"
+            rel="noopener noreferrer"
+            target="_blank"
+            passHref
+            className="underline transition-colors duration-300 hover:bg-main-color"
+          >
+            중앙 해커톤에 참가하여 웹 서비스를 구축하는 경험을 하였습니다.
+          </Link>,
         ]}
       />
     </div>
