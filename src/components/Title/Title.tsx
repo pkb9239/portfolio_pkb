f1a3d7781d225b777499a4d22f7c960418b1bd8e
Kyungbin <pkb8839@naver.com>
2024-05-01 15:21:43 +0900

    ✨feat : DotPoint 컴포넌트 추가
    기존의 Title 컴포넌트에 DotPoint 컴포넌트를 추가하였습니다.

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,8 +1,13 @@
+import DotPoint from '../DotPoint';
+
 const Title = ({ text }: { text: string }) => {
   return (
     <>
       <div className="flex flex-col md:gap-6 gap-1">
-        <div className="md:text-7xl text-3xl">{text}</div>
+        <div className="md:text-7xl text-3xl">
+          {text}
+          <DotPoint />
+        </div>
         <div className="w-full md:border-t-8 border-t-4 border-slate-600"></div>
       </div>
     </>
