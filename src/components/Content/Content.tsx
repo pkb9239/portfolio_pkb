1d9feaa60ee1e519e96358f446c9437c30bd1d79
Kyungbin <pkb8839@naver.com>
2024-05-02 17:29:13 +0900

    💄design : Content 컴포넌트 스타일링
    - 모바일을 고려한 스타일링을 진행하였습니다. (글자 크기, 간격 등등)
    - 필요한 부분에는 isMobile hook을 이용하였습니다.

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,7 +1,10 @@
+'use client';
+
 import { ReactNode } from 'react';
 import { FaCheck } from 'react-icons/fa';
 import { RiAttachment2 } from 'react-icons/ri';
 import { BsStack } from 'react-icons/bs';
+import useResize from '@/hooks/useResize';
 interface ContentProps {
   category: string;
   title: ReactNode;
@@ -21,32 +24,33 @@ const Content = ({
   skill,
   links,
 }: ContentProps) => {
+  const { isMobile } = useResize();
   return (
     <>
-      <div className="flex flex-col gap-4">
-        <div className="text-2xl font-medium">{category}</div>
-        <div className="text-4xl font-bold">{title}</div>
+      <div className="flex flex-col md:gap-4 gap-3">
+        <div className="md:text-2xl text-xl font-medium">{category}</div>
+        <div className="md:text-4xl text-2xl font-bold">{title}</div>
         {period && (
           <div>
-            <div className="text-lg">{period}</div>
+            <div className="md:text-lg text-base">{period}</div>
           </div>
         )}
         {introduce && (
           <div>
-            <div className="text-lg font-medium">{introduce}</div>
+            <div className="md:text-lg text-base font-medium">{introduce}</div>
           </div>
         )}
         <div>
           <ul>
             {content.map((item, index) => (
               <li
-                className="flex gap-2 text-xl items-center pl-2"
+                className="flex gap-2 md:text-xl text-base items-center md:pl-2 pl-1"
                 key={index}
               >
                 <span>
                   <FaCheck
                     color="#3b81f6"
-                    size={20}
+                    size={isMobile ? 14 : 20}
                   />
                 </span>
                 {item}
@@ -56,7 +60,7 @@ const Content = ({
         </div>
         {links && (
           <div>
-            <div className="flex items-center text-2xl gap-1 font-bold pb-4">
+            <div className="flex items-center md:text-2xl text-lg gap-1 font-bold md:pb-4 pb-2">
               <span>
                 <RiAttachment2 size={24} />
               </span>
@@ -66,13 +70,13 @@ const Content = ({
               {links &&
                 links.map((item, index) => (
                   <li
-                    className="flex gap-2 text-xl items-center pl-2"
+                    className="flex gap-2 md:text-xl text-base items-center md:pl-2 pl-1"
                     key={index}
                   >
                     <span>
                       <FaCheck
                         color="#3b81f6"
-                        size={20}
+                        size={isMobile ? 14 : 20}
                       />
                     </span>
                     {item}
@@ -83,7 +87,7 @@ const Content = ({
         )}
         {skill && (
           <div>
-            <div className="flex items-center gap-1 text-2xl font-bold pb-4">
+            <div className="flex items-center gap-1 md:text-2xl text-lg font-bold pb-4">
               <span>
                 <BsStack size={22} />
               </span>
