b4a90ee6401e22370623876d404d0ace475e9a90
Kyungbin <pkb8839@naver.com>
2024-05-13 18:08:23 +0900

    💄design : Header 스타일링 수정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -32,11 +32,14 @@ const Header = () => {
         <div className="absolute inset-0 backdrop-opacity-10 backdrop-invert bg-white/30"></div>
         <div className="flex flex-col w-full h-full relative z-10 items-center justify-center">
           <div className="flex flex-col items-center justify-center md:gap-10 gap-1.5 font-semibold">
-            <div className="flex gap-1">
+            <div className="flex items-center gap-1">
               <div className="animate-[wiggle_1s_ease-in-out_infinite] md:text-3xl text-xl font-semibold">
-                👋🏻
+                👋🏻&nbsp;
               </div>
-              <div className="md:text-4xl text-xl"> 안녕하세요.</div>
+              <div className="md:text-4xl text-2xl">
+                안녕하세요
+                <span className="md:text-5xl text-xl text-main-color">.</span>
+              </div>
             </div>
             <p className="md:text-4xl text-xl">🌳 울창한 숲을 목표로</p>
             <p className="md:text-4xl text-xl">
@@ -47,7 +50,8 @@ const Header = () => {
               <span className="md:text-6xl text-4xl underline decoration-5 decoration-sky-500">
                 {name}
               </span>
-              입니다.
+              입니다
+              <span className="md:text-5xl text-2xl text-main-color">.</span>
             </p>
             <Link
               to="project"
