e055a9c36e1a82736dddbe32aeb57ad430c5ac80
Kyungbin <pkb8839@naver.com>
2024-07-17 16:00:02 +0900

    ✨feat : MouseFollower 컴포넌트 적용
    각 페이지에 MouseFollower 컴포넌트를 적용하였습니다.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,45 @@
 import Education from '@/components/Education';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
+import MouseFollower from '@/components/MouseFollower';
 import NavBar from '@/components/NavBar';
 import Project from '@/components/Project';
 import ScrollTopButton from '@/components/ScrollTopButton';
 import Study from '@/components/Study';
 
-export default function Home() {
+const Home = () => {
   return (
     <main>
-      <NavBar />
-      <Header />
-      <nav className="fixed md:right-12 md:bottom-12 right-5 bottom-5 flex justify-around items-center w-12 h-12 z-50">
-        <ScrollTopButton />
-      </nav>
-      <section className="flex flex-col items-center md:py-16 py-0 md:gap-16 gap-6">
-        <article
-          id="project"
-          className="flex flex-col md:w-3/5 w-full md:px-0 px-4"
-        >
-          <Project />
-        </article>
-        <article
-          id="education"
-          className="flex flex-col md:w-3/5 w-full md:px-0 px-4"
-        >
-          <Education />
-        </article>
-        <article
-          id="study"
-          className="flex flex-col md:w-3/5 w-full md:px-0 px-4"
-        >
-          <Study />
-        </article>
-      </section>
-      <Footer />
+      <MouseFollower>
+        <NavBar />
+        <Header />
+        <nav className="fixed md:right-12 md:bottom-12 right-5 bottom-5 flex justify-around items-center w-12 h-12 z-50">
+          <ScrollTopButton />
+        </nav>
+        <section className="flex flex-col items-center md:py-16 py-0 md:gap-16 gap-6">
+          <article
+            id="project"
+            className="flex flex-col md:w-3/5 w-full md:px-0 px-4"
+          >
+            <Project />
+          </article>
+          <article
+            id="education"
+            className="flex flex-col md:w-3/5 w-full md:px-0 px-4"
+          >
+            <Education />
+          </article>
+          <article
+            id="study"
+            className="flex flex-col md:w-3/5 w-full md:px-0 px-4"
+          >
+            <Study />
+          </article>
+        </section>
+        <Footer />
+      </MouseFollower>
     </main>
   );
-}
+};
+
+export default Home;
